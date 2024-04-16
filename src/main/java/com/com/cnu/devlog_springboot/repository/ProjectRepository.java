c5f143f327619cffc6d91f4f4caab960d0cd621b
202002536JHLee <earlylee2@gmail.com>
2024-04-16 22:35:35 +0900

    project

--- src/main/java/com/com/cnu/devlog_springboot/repository/ProjectRepository.java
+++ src/main/java/com/com/cnu/devlog_springboot/repository/ProjectRepository.java
@@ -1,11 +1,11 @@
 package com.com.cnu.devlog_springboot.repository;
 
-import com.com.cnu.devlog_springboot.model.Post;
+import com.com.cnu.devlog_springboot.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Integer> {
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
 //    Post save(Post post);
 //    List<Post> findAll();
 //    Optional<Post> findById(Integer postId);
